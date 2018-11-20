dcd330b90b8c6c82473178a6e5c5ae3341fefceb
wb <1274481905@qq.com>
2018-11-20 15:07:09 +0800

    test ignore

---
+++ src/main/java/com/chinasoft/yanfa/SpringGitTestApplication.java
@@ -0,0 +1,12 @@
+package com.chinasoft.yanfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitTestApplication.class, args);
+	}
+}
