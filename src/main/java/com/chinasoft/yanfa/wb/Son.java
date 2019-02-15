bd5e4fe9c6e763608630eddd16594afc98410f29
wb <1274481905@qq.com>
2019-02-15 16:44:33 +0800

    son commit

---
+++ src/main/java/com/chinasoft/yanfa/wb/Son.java
@@ -0,0 +1,12 @@
+package com.chinasoft.yanfa.wb;
+
+/**
+ * @author wb
+ * @create 2019-02-15 16:43
+ */
+public class Son {
+
+    public static void main(String[] args) {
+        System.out.println("xxxxxxxxxxxxxxxx");
+    }
+}
