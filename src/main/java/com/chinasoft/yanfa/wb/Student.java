229783e9afec3c210d7da94f0bf60bd5f4dee6cb
wb <1274481905@qq.com>
2019-02-15 14:27:23 +0800

    test git 1

---
+++ src/main/java/com/chinasoft/yanfa/wb/Student.java
@@ -0,0 +1,19 @@
+package com.chinasoft.yanfa.wb;
+
+/**
+ * @author wb
+ * @create 2019-02-15 14:15
+ */
+public class Student {
+
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
